3ca73c092f93c52eb22cc6a9bda510d66b83bcbd
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-04-23 12:59:23 -0300

    Hello World! + PseudoCode Rock-Paper-Scissors

---
+++ javascript.js
@@ -0,0 +1,28 @@
+/*
+PSEUDO-CODE
+Paso A = Definir "Piedra" "Papel" y "Tijera".
+
+Tijera > papel
+Papel > piedra
+Piedra > tijera
+
+Paso B = La computadora elegirá aleatoriamente uno de los 3
+
+Podría crear una función que aleatoriamente devuelva una de las 3 opciones.
+
+E.J
+
+function getComputerChoice() = 
+Paso C = El jugador debe elegir uno de los 3 // prompt("¿Piedra, Papel o Tijera?" parseInt("")); 
+
+ParseInt para que luego pueda ser evaluado.
+
+Paso D = Generar los posibles resultados evaluando la elección de ambos jugadores.
+//
+
+If {P1 > PC console.log("You win");}
+Else if {P1 < PC console.log("CPU win");}
+Else {console.log("Empate");}
+*/
+
+console.log("Hello World!");
