672481d225d20de1bc512ef5f1a0f3cc2f055077
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-04-23 16:09:39 -0300

    Function randomly picks rock, paper or scissors

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ Podría crear una función que aleatoriamente devuelva una de las 3 opciones.
 E.J
 
 function getComputerChoice() = 
+
 Paso C = El jugador debe elegir uno de los 3 // prompt("¿Piedra, Papel o Tijera?" parseInt("")); 
 
 ParseInt para que luego pueda ser evaluado.
@@ -25,4 +26,18 @@ Else if {P1 < PC console.log("CPU win");}
 Else {console.log("Empate");}
 */
 
-console.log("Hello World!");
+
+// PASO B
+
+const options = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+const storeComputerChoice = Math.floor(Math.random() * options.length); // Pick a random number between 0 and 2 which is options.length.
+return options[storeComputerChoice]; // Return that number into the function
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
